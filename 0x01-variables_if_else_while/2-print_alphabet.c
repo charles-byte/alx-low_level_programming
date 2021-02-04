d16b7167bed3d163e8dd5cef8d4be13667509dd2
charles-byte <theobonyo@yahoo.com>
2021-02-04 17:17:01 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
  *main - prints the alphabet in lower case
  *@void: empty parameter list for main.
  *
- *Description: Prints the alphabet in lower case 
- *Can only use putchar
- *
+ *Description: Prints the alphabet in lower case
+*Can only use putchar
+*
  *Return: 0 for success
  */
 
