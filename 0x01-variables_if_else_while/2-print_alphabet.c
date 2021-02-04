dde7ae0fea36607aaba76fdcd53ce2d6f07ab4e2
charles-byte <theobonyo@yahoo.com>
2021-02-04 17:06:57 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *main - prints the alphabet in lower case
+ *@void: empty parameter list for main.
+ *
+ *Description: Prints the alphabet in lower case 
+ *Can only use putchar
+ *
+ *Return: 0 for success
+ */
+
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
