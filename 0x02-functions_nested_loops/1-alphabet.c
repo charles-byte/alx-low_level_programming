a199c0ec9b53b584168bdaf3f0c2bb8167e85769
charles-byte <theobonyo@yahoo.com>
2021-02-09 17:44:53 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ **/
+
+void print_alphabet(void)
+{
+
+char t;
+
+for (t = 'a'; t <= 'z'; t++)
+{
+_putchar(t);
+}
+_putchar('\n');
+}
+
