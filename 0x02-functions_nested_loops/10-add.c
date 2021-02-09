3810f9a0deec8f8b7afd36d32e716a6ace1fd016
charles-byte <theobonyo@yahoo.com>
2021-02-09 19:05:21 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+*
+ * @k: int one
+ * @m: int two
+ * Return: sum of int one/int two
+ */
+int add(int k, int m)
+{
+k += m;
+return (k);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints times table up to 9
+ *
+ * Return: void
+ *
+ **/
+
+void times_table(void)
