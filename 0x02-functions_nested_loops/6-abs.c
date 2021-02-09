2ff785773221432266ee25d4d93b06d2ada03fc6
charles-byte <theobonyo@yahoo.com>
2021-02-09 18:32:02 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+
+/**
+ * _abs - compute absolute value
+ * @n: int being computed
+ * Return: absolute value
+ **/
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
