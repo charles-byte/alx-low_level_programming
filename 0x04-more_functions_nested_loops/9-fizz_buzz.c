46b7f920fb95acb723f9c996c22dfff8b955ffa7
charles-byte <theobonyo@yahoo.com>
2021-02-11 20:55:38 +0000

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - test
+ *
+ *Return: zero
+ */
+int main(void)
+{
+int k;
+for (k = 1; k <= 100; k++)
+{
+if (k % 3 == 0 && k % 5 == 0)
+printf("FizzBuzz ");
+else if (k % 3 == 0)
+printf("Fizz ");
+else if (k % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", k);
+}
+return (0);
+}
