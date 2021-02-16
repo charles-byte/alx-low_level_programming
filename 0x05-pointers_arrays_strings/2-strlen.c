86a3d8205a7e1fe13b32fe8cddfe73ad8f3101fb
charles-byte <theobonyo@yahoo.com>
2021-02-16 18:07:01 +0000

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: input string
+ * Return: length of string as int
+ **/
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+
+i++;
+}
+return (i);
+}
