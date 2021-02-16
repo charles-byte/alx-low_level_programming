ab1fce97e27bb5dda9d4da305ae2e82002cbb8df
charles-byte <theobonyo@yahoo.com>
2021-02-16 17:56:10 +0000

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - set value to 98
+ * @n: address of element change
+ * Description: resets *n to 98
+ * Return: nothing
+ **/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
