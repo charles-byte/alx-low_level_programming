c78835be5f550f677d84d225bd1ef41eb5772aa9
charles-byte <theobonyo@yahoo.com>
2021-02-18 19:57:46 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * leet - sw33t 1337 sp34k g3n3r4t0r
+ *
+ * @s: string to convert
+* Return: returned value
+ */
+char *leet(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] != '\0')
+{
+while (s[i] == 'a' || s[i] == 'A')
+s[i] = '4';
+while (s[i] == 'e' || s[i] == 'E')
+s[i] = '3';
+while (s[i] == 'o' || s[i] == 'O')
+s[i] = '0';
+while (s[i] == 't' || s[i] == 'T')
+s[i] = '7';
+while (s[i] == 'l' || s[i] == 'L')
+s[i] = '1';
+i++;
+}
+return (s);
+}
