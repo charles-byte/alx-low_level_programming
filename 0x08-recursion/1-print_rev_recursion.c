a036a5bf0042b534327705bc972206144420f7f9
charles-byte <theobonyo@yahoo.com>
2021-02-24 20:27:30 +0000

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - prints in reverse
+ *@s: string to print
+ *return: zero
+ */
+
+void _print_rev_recursion(char *s);
+{
+
+if (s[0] != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(s[0]);
+}
+}
